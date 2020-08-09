8da9fbc8ea175b9321351165a3720fe4566c6b38
sajjad1386116 <67165936+sajjad1386116@users.noreply.github.com>
2020-08-09 17:47:53 +0430

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'db',
         'USER': 'sajjad',
         'PASSWORD': 'sajjad',
